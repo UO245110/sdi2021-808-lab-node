6cf77f21847a88a281da08028fa0d15d32f4730b
Xurde <uo245110@uniovi.es>
2021-04-19 22:55:49 +0200

    SDI-2021-808-10.3-SW Cliente JQuery + AJAX

--- app.js
+++ app.js
@@ -3,6 +3,16 @@
 let express = require('express');
 let app = express();
 
+//Esto es para hacer la aplicación más permisisva, no tengo ni idea.
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+    // Debemos especificar todas las headers que se aceptan. Content-Type , token
+    next();
+});
+
 //https://www.npmjs.com/package/jsonwebtoken SEGURIDAD DE TOKEN
 let jwt = require('jsonwebtoken');
 app.set('jwt',jwt);
